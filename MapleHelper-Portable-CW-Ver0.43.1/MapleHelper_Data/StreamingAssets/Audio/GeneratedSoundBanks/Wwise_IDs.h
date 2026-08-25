a3841a63fb60f261ebe5475ee937dbe9fbc309f1
ReezC <reezc9586@gmail.com>
2026-08-26 00:17:52 +0800

    Refactor code structure for improved readability and maintainability

---
+++ MapleHelper-Portable-CW-Ver0.43.1/MapleHelper_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BOXRESULT = 2573674595U;
+        static const AkUniqueID BTNMOUSECLICK = 2748566804U;
+        static const AkUniqueID BTNMOUSEOVER = 2725359730U;
+        static const AkUniqueID BUYSHOPITEM = 1777102910U;
+        static const AkUniqueID DLGNOTICE = 271096388U;
+        static const AkUniqueID ENCHANT = 2119252428U;
+        static const AkUniqueID ENCHANTDESTROYED = 3544254253U;
+        static const AkUniqueID ENCHANTFAIL = 3133948648U;
+        static const AkUniqueID ENCHANTREPLACE = 1340731732U;
+        static const AkUniqueID ENCHANTSUCCESS = 3886522645U;
+        static const AkUniqueID INVITE = 3031509150U;
+        static const AkUniqueID LOGO = 556785088U;
+        static const AkUniqueID MENUDOWN = 1914916084U;
+        static const AkUniqueID MENUUP = 172993487U;
+        static const AkUniqueID ROLLDOWN = 1832278084U;
+        static const AkUniqueID ROLLUP = 27771263U;
+        static const AkUniqueID SCROLLUP = 2546189601U;
+        static const AkUniqueID TAB = 845985362U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUTE_MAIN
+        {
+            static const AkUniqueID GROUP = 1532643310U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NO = 1668749452U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID YES = 979470758U;
+            } // namespace STATE
+        } // namespace MUTE_MAIN
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOLUME_MAIN = 3747206233U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
